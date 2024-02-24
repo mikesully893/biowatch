2ca4ebc31ecc55551fc97913821c7c91bcb9339a
mikesully893 <msully893@gmail.com>
2024-02-24 11:02:12 +0000

    move keywords to config.txt file

--- src/main.rs
+++ src/main.rs
@@ -4,17 +4,19 @@ struct PressReleaseLink {
     url: String,
 }
 
-fn keyword_check(url: &String) -> bool {
-    let keywords = ["Phase-1", "Phase-I", "Phase-2", "Phase-II", "Preliminary"];
-    for word in keywords {
-        if url.contains(&word) {
+fn keyword_check(url: &str) -> bool {
+    let config_text = include_str!("config.txt");
+    let keywords: Vec<String> = config_text.lines().map(|s| s.to_string()).collect();
+
+    for word in &keywords {
+        if url.contains(word) {
             return true
         };
     };
     false
 }
 
-//fn add_pressrelease_link(url: String)
+
 
 fn main() {
     let response = blocking::get("https://ir.vikingtherapeutics.com/press-releases",)
@@ -43,7 +45,6 @@ fn main() {
             press_releases.push(press_release_link);
         }
     }
-
     for (index, release) in press_releases.iter().enumerate() {
         println!("Press Release #{}", index + 1);
         println!("Url: {:?}", release.url);
